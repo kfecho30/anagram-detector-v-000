3964c0929c7afa84b07e268d967966f8f489d20a
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 14:44:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,12 @@
 class Anagram
   attr_accessor :word
+  @final = []
   def initialize(word)
     @word = word
   end
   def match(arr)
+    arr.each do |poss|
+      poss.split("").sort == word.split("").sort ? @final << poss
+    end
   end
 end
